6017b13bba19e5839a4bff13998d417232262676
Mohammad MDSA <samimd.77@gmail.com>
2017-11-23 08:34:50 +0330

    Added regex routing

--- app.js
+++ app.js
@@ -18,3 +18,41 @@ app.all('/samimd', (req, res, next) => {
 	res.send('Accessing the SamiMD section...');
 	next();
 });
+
+
+// Regex routing
+app.get('/', (req, res) => {
+	res.send('root');
+});
+
+app.get('/about', (req, res) => {
+	res.send('about');
+});
+
+app.get('/random.text', (req, res) => {
+	res.send('random.text');
+});
+
+app.get('/ab?cd', (req, res) => {
+	res.send('ab?cd');
+});
+
+app.get('/ab+cd', (req, res) => {
+	res.send('ab+cd');
+});
+
+app.get('/ab*cd', (req, res) => {
+	res.send('ab*cd');
+});
+
+app.get('/ab(cd)?e', (req, res) => {
+	res.send('ab(cd)?e');
+});
+
+app.get(/a/, (req, res) => {
+	res.send('/a/');
+});
+
+app.get(/.*fly$/, (req, res) => {
+	res.send('/.*fly$/');
+});
