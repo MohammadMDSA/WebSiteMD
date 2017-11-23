027bc0febf8d233b43a5beace7b7f509c5669581
Mohammad MDSA <samimd.77@gmail.com>
2017-11-23 11:16:16 +0330

    Add simple html serving server

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 3000;
+const fs = require('fs');
+
+app.listen(port, () => {
+	console.log(`Server listening to ${port}.`);
+});
+
+app.use((req, res, next) => {
+	console.log(`${Date.now}\t${req.url}`);
+	next();
+});
+
+app.route('/')
+	.get((req, res) => {
+		res.send(html);
+	});
+
+let html = fs.readFileSync('index.html');
